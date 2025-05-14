9b79fefb5be9850839d4055eb7e98e7ed1b85f39
thuankg1752 <thuankg1752@gmail.com>
2025-05-15 04:18:16 +0700

    feat(entity): add TaskStatus enum and update TaskEntity to use it for status management

--- TaskManagement/src/main/java/com/uit/tms/TaskManagement/entity/TaskEntity.java
+++ TaskManagement/src/main/java/com/uit/tms/TaskManagement/entity/TaskEntity.java
@@ -2,20 +2,13 @@ package com.uit.tms.TaskManagement.entity;
 
 import java.time.LocalDateTime;
 
+import com.uit.tms.TaskManagement.enums.TaskStatus;
+import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -38,9 +31,10 @@ public class TaskEntity {
     private String title;
     
     private String description;
-    
-    private String status;
-    
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
     private float estTime;
     
     private LocalDateTime dueDate;
