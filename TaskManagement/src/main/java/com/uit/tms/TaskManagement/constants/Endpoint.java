b108dd59513f3050e459fb0e083e0c0465301b93
Tan Nguyen <tannp27@gmail.com>
2025-05-14 16:05:20 +0700

    fix(endpoint): update endpoint and improve makefile for Maven build

--- TaskManagement/src/main/java/com/uit/tms/TaskManagement/constants/Endpoint.java
+++ TaskManagement/src/main/java/com/uit/tms/TaskManagement/constants/Endpoint.java
@@ -2,11 +2,14 @@ package com.uit.tms.TaskManagement.constants;
 
 public class Endpoint {
     public static final String HOME = "/";
+
     public static final String TASKS = "/tasks";
     public static final String TASK_ID = "/task/{id}";
     public static final String TASK_CREATE = "/task/create";
+
     public static final String LOGIN = "/login";
-    public static final String USER_GITHUB = "/user/github";
     public static final String LOGOUT = "/logout";
     public static final String REGISTER = "/register";
+
+    public static final String USER_GITHUB = "/user/github";
 }
